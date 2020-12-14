710f13524e23f7a44fbb718f10f06db86a1a3e3a
zacaugustzac <zacaugustzac@gmail.com>
2020-12-14 18:27:11 +0800

    CustomerImplementation

--- src/main/java/sg/edu/iss/test/repo/CustomerRepository.java
+++ src/main/java/sg/edu/iss/test/repo/CustomerRepository.java
@@ -3,7 +3,8 @@ package sg.edu.iss.test.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sg.edu.iss.test.model.Customer;
+import sg.edu.iss.test.model.User;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+	public Customer findByName(String un);
 }
---
+++ src/main/java/sg/edu/iss/test/service/CustomerImplementation.java
@@ -0,0 +1,40 @@
+package sg.edu.iss.test.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import sg.edu.iss.test.model.Customer;
+import sg.edu.iss.test.repo.CustomerRepository;
+
+public class CustomerImplementation implements CustomerInterface {
+
+	@Autowired
+	CustomerRepository crepo;
+	
+	@Override
+	public void createCustomer(Customer customer) {
+		crepo.save(customer);
+	}
+
+	@Override
+	public void updateCustomer(Customer customer) {
+		crepo.save(customer);	
+	}
+
+	@Override
+	public List<Customer> listAllCustomer() {
+		return crepo.findAll();
+	}
+
+	@Override
+	public void deleteCustomer(Customer customer) {
+		crepo.delete(customer);
+	}
+
+	@Override
+	public Customer findByName(String name) {
+		return crepo.findByName(name);
+	}
+
+}
