60ad4a39cb36b164afbd0d9073fa8fb8316ad436
lkaditya <69144603+lkaditya@users.noreply.github.com>
2020-12-14 15:36:00 +0800

    add customer repository and test customer table creation

--- src/test/java/sg/edu/iss/test/EtoeApplicationTests.java
+++ src/test/java/sg/edu/iss/test/EtoeApplicationTests.java
@@ -4,14 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import sg.edu.iss.test.model.Customer;
 import sg.edu.iss.test.model.RoleType;
 import sg.edu.iss.test.model.User;
+import sg.edu.iss.test.repo.CustomerRepository;
 import sg.edu.iss.test.repo.UserRepository;
 
 @SpringBootTest
 class EtoeApplicationTests {
 	@Autowired
 	UserRepository urepo;
+	@Autowired
+	CustomerRepository cusrepo;
+	
 
 	@Test
 	void contextLoads() {
@@ -19,5 +24,14 @@ class EtoeApplicationTests {
 		urepo.save(u1);
 		
 	}
+	
+	@Test
+	void loadCustomers() {
+		Customer a=new Customer("Alpha",34562283);
+		cusrepo.save(a);
+		
+		
+	}
+	
 
 }
