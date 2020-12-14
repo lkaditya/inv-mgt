b3e1af9a496b57873cee2aa00122fcf8794efa6b
lkaditya <69144603+lkaditya@users.noreply.github.com>
2020-12-14 16:01:34 +0800

    add the customer repository and make test for data seeding

--- src/test/java/sg/edu/iss/test/EtoeApplicationTests.java
+++ src/test/java/sg/edu/iss/test/EtoeApplicationTests.java
@@ -1,13 +1,17 @@
 package sg.edu.iss.test;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import sg.edu.iss.test.model.Customer;
+import sg.edu.iss.test.model.RepairOrder;
 import sg.edu.iss.test.model.RoleType;
 import sg.edu.iss.test.model.User;
 import sg.edu.iss.test.repo.CustomerRepository;
+import sg.edu.iss.test.repo.RepairOrderRepository;
 import sg.edu.iss.test.repo.UserRepository;
 
 @SpringBootTest
@@ -16,6 +20,8 @@ class EtoeApplicationTests {
 	UserRepository urepo;
 	@Autowired
 	CustomerRepository cusrepo;
+	@Autowired
+	RepairOrderRepository repairrepo;
 	
 
 	@Test
@@ -29,8 +35,28 @@ class EtoeApplicationTests {
 	void loadCustomers() {
 		Customer a=new Customer("Alpha",34562283);
 		cusrepo.save(a);
-		
-		
+		Customer b=new Customer("Beta",84349210);
+		cusrepo.save(b);
+		Customer c=new Customer("Charlie",23894012);
+		cusrepo.save(c);
+		Customer d=new Customer("Delta",99749329);
+		cusrepo.save(d);	
+	}
+	
+	@Test
+	void loadRepairOrder() {
+		RepairOrder rep1=new RepairOrder();
+		Customer a= cusrepo.findCustomerByName("Alpha").get(0);
+		Customer b= cusrepo.findCustomerByName("Beta").get(0);
+		Customer c= cusrepo.findCustomerByName("Charlie").get(0);
+		Customer d= cusrepo.findCustomerByName("Delta").get(0);
+		rep1.setCustomer(a);
+		LocalDate d1=LocalDate.now();
+		rep1.setRepairDate(d1);
+		repairrepo.save(rep1);
+	
+
+	
 	}
 	
 
