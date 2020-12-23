20ee8f649afb23fcc514d8f6f0ccec45935c9ce5
Sharon <locky.lockandkey@gmail.com>
2020-12-23 15:46:36 +0800

    Pricing must be integer

--- src/main/java/sg/edu/iss/test/model/Inventory.java
+++ src/main/java/sg/edu/iss/test/model/Inventory.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.Digits;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,16 +25,19 @@ public class Inventory {
 	private long qoh;
 	//reorder level
 	private long rol;
-	private String wholesale;
+	@Digits(integer=6, fraction=2)
+	private double wholesale;
 	//price of retail
-	private String por;
+	@Digits(integer=6, fraction=2)
+	private double por;
 	//price of partner
-	private String pop;
+	@Digits(integer=6, fraction=2)
+	private double pop;
     @OneToOne(mappedBy = "inventory")
     private Product product;
     @OneToMany(mappedBy="inventory")
     private Collection<Returned> returned;
-	public Inventory(long id, long qoh, long rol, String wholesale, String por, String pop, Product product,
+	public Inventory(long id, long qoh, long rol, @Digits(integer = 6, fraction = 2) double wholesale, @Digits(integer = 6, fraction = 2) double por, @Digits(integer = 6, fraction = 2) double pop, Product product,
 			Collection<Returned> returned) {
 		super();
 		this.id = id;
